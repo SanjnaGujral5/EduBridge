3df68ddb6eb6d33dc17dbe3cc40f65d82468082c
Sanjna Gujral <sanjnagujral01@Sanjnas-MacBook-Air.local>
2025-12-16 19:42:04 +0530

    frontend: prepare for vercel deployment

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,13 +3,17 @@ import { assets } from "../assets/assets";
 import { dummyCourses } from "../assets/assets";
 import {useNavigate} from "react-router-dom";
 import humanizeDuration from 'humanize-duration'
+import { useAuth, useUser } from "@clerk/clerk-react"
 
- export const AppContext = createContext()
+export const AppContext = createContext()
 
 export const AppContextProvider = (props)=>{
 
     const currency = import.meta.env.VITE_CURRENCY
     const navigate = useNavigate()
+
+    const {getToken} = useAuth()
+    const {user} = useUser()
     const [allCourses, setALLCourses] = useState([])
     const [isEducator, setIsEducator] = useState(true)
     const [enrolledCourses, setEnoralledCourses] = useState([])
@@ -69,6 +73,16 @@ export const AppContextProvider = (props)=>{
         fetchAllCourses();
         fetchUserEnrolledCourses();
     },[])
+
+    const logToken = async ()=>{
+        console.log(await getToken());
+    }
+
+    useEffect(()=>{
+        if(user){
+            logToken()
+        }
+    },[user])
     const value = {
         currency, allCourses, navigate, calculateRating,
         isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration,
